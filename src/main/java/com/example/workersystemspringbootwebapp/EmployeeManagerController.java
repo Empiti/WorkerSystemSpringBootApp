576f38b2c43f2c08f93985b2ec92eda138e1c110
Ksawery Kasprzycki <ksawery.kasprzyckix@gmail.com>
2023-05-19 14:25:45 +0200

    Stworzono GetMapping wyświetlający index oraz stworzono liste

--- src/main/java/com/example/workersystemspringbootwebapp/EmployeeManagerController.java
+++ src/main/java/com/example/workersystemspringbootwebapp/EmployeeManagerController.java
@@ -1,4 +1,17 @@
 package com.example.workersystemspringbootwebapp;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmployeeManagerController {
+
+    private List<Employee> employees = new ArrayList<>();
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("employees", employees);
+        return "index";
+    }
 }
