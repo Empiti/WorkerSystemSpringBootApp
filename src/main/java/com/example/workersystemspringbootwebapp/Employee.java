0169850c48a7ba8b37ccb4f5ded1d40c73001ca4
Ksawery Kasprzycki <ksawery.kasprzyckix@gmail.com>
2023-05-19 14:20:22 +0200

    Stworzono zmienne i konstruktor

--- src/main/java/com/example/workersystemspringbootwebapp/Employee.java
+++ src/main/java/com/example/workersystemspringbootwebapp/Employee.java
@@ -1,4 +1,20 @@
 package com.example.workersystemspringbootwebapp;
 
 public class Employee {
+    private int id;
+    private String name;
+    private String lastName;
+    private String position;
+    private int salary;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String name, String lastName, String position, int salary) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.position = position;
+        this.salary = salary;
+    }
 }
