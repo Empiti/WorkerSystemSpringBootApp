497efa972a0bc746178ae1e9dff17a5270145b04
Ksawery Kasprzycki <ksawery.kasprzyckix@gmail.com>
2023-05-19 12:12:33 +0200

    Stworzono nowy projekt Spring Boot

---
+++ src/main/java/com/example/workersystemspringbootwebapp/WorkerSystemSpringBootWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.workersystemspringbootwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerSystemSpringBootWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkerSystemSpringBootWebAppApplication.class, args);
+    }
+
+}
