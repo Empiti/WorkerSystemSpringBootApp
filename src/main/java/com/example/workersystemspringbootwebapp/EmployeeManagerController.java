024276586b0cd47c8eca6b7e0ea1b9360514efd3
Ksawery Kasprzycki <ksawery.kasprzyckix@gmail.com>
2023-05-19 14:27:54 +0200

    Stworzono GetMapping oraz PostMapping odpowiedzialne za wyświetlanie strony z formularzem dodawania oraz za dodawanie pracowników do listy

--- src/main/java/com/example/workersystemspringbootwebapp/EmployeeManagerController.java
+++ src/main/java/com/example/workersystemspringbootwebapp/EmployeeManagerController.java
@@ -2,6 +2,8 @@ package com.example.workersystemspringbootwebapp;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,5 +15,18 @@ public class EmployeeManagerController {
     public String index(Model model) {
         model.addAttribute("employees", employees);
         return "index";
+    }
+    private int id = 1;
+    @GetMapping("/add")
+    public String showAddForm(Model model) {
+        model.addAttribute("employee", new Employee());
+        return "add-form";
+    }
+
+    @PostMapping("/add")
+    public String addEmployee(@ModelAttribute Employee employee) {
+        employee.setId(id++);
+        employees.add(employee);
+        return "redirect:/";
     }
 }
