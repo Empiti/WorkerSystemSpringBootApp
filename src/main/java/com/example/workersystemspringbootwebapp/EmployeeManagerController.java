7bb642284bdbef1d08ff87214b7d11a0e4a2f54e
Ksawery Kasprzycki <ksawery.kasprzyckix@gmail.com>
2023-05-19 14:31:36 +0200

    Stworzono GetMapping oraz PostMapping odpowidzialne za wyświetlanie strony z edytowaniem pracowników oraz za tego funkcjonalnością oraz stworzono metode która wyszukuje pracownika o określonym ID

--- src/main/java/com/example/workersystemspringbootwebapp/EmployeeManagerController.java
+++ src/main/java/com/example/workersystemspringbootwebapp/EmployeeManagerController.java
@@ -3,6 +3,7 @@ package com.example.workersystemspringbootwebapp;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -27,6 +28,29 @@ public class EmployeeManagerController {
     public String addEmployee(@ModelAttribute Employee employee) {
         employee.setId(id++);
         employees.add(employee);
+        return "redirect:/";
+    }
+
+    private Employee findEmployeeById(int id) {
+        for (Employee employee : employees) {
+            if (employee.getId() == id) {
+                return employee;
+            }
+        }
+        return null;
+    }
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable("id") int id, Model model) {
+        Employee employee = findEmployeeById(id);
+        model.addAttribute("employee", employee);
+        return "edit-form";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String editEmployee(@PathVariable("id") int id, @ModelAttribute Employee updatedEmployee) {
+        Employee employee = findEmployeeById(id);
+        employee.setPosition(updatedEmployee.getPosition());
+        employee.setSalary(updatedEmployee.getSalary());
         return "redirect:/";
     }
 }
